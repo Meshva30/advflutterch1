fa888f710a325d397d5ddd8144965a654f9ff86d
Dell <5095meshwapatel@email.com>
2024-04-26 10:31:07 +0530

    Radhey Radhey

--- lib/stepper/view/screen/stepper2.dart
+++ lib/stepper/view/screen/stepper2.dart
@@ -14,24 +14,33 @@ class _Stepper2State extends State<Stepper2> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
+        title:  Text(
           'Flutter Stepper Demo',
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
         ),
-
+        backgroundColor: Colors.blue,
       ),
       body: Theme(
         data: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue,backgroundColor: Colors.white),
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.blue,
+            backgroundColor: Colors.white,
+          ),
         ),
-        child:Stepper(
-          onStepTapped: (value) {
+        child: Stepper(
+          elevation: 1,
+          type: StepperType.horizontal,
+          currentStep: Stepperglobalvalue.StepperIndex,
+          onStepContinue: () {
             setState(() {
-              Stepperglobalvalue.StepperIndex = value;
+              if (Stepperglobalvalue.StepperIndex ==
+                  Stepperglobalvalue.StepperIndexStop) {
+                Stepperglobalvalue.StepperIndex == 2;
+              } else {
+                Stepperglobalvalue.StepperIndex++;
+              }
             });
           },
-          currentStep: Stepperglobalvalue.StepperIndex,
           onStepCancel: () {
             setState(() {
               if (Stepperglobalvalue.StepperIndex == 2) {
@@ -41,71 +50,104 @@ class _Stepper2State extends State<Stepper2> {
               }
             });
           },
-          onStepContinue: () {
-            setState(() {
-              if (Stepperglobalvalue.StepperIndex ==
-                  Stepperglobalvalue.StepperIndexStop) {
-                Stepperglobalvalue.StepperIndex = 2;
-              } else {
-                Stepperglobalvalue.StepperIndex++;
-              }
-            });
-          },
-
-
-         stepIconBuilder: (stepIndex, stepState) {
-            return CircleAvatar(
+          stepIconBuilder: (stepIndex, stepState) {
+            return const CircleAvatar(
               backgroundColor: Colors.blue,
               child: Icon(
                 Icons.done,
-                size: 20,
                 color: Colors.white,
+                size: 20,
               ),
             );
           },
-
-          type: StepperType.horizontal,
-          steps: [
+          steps: const [
             Step(
               title: Text('Personal'),
-              content: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  hintText: 'Email',
-                  prefixIcon: Icon(
-                    Icons.email,
-                    color: Colors.grey,
+              content: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      labelText: 'Enter Your Fist Name',
+                      prefixIcon: Icon(Icons.person),
+                    ),
                   ),
-                ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Your Last Name',
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             Step(
-                title: Text('Contact'),
-                content: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    hintText: 'Address',
-                    prefixIcon: Icon(
-                      Icons.home,
-                      color: Colors.grey,
+              title: Text('Contact'),
+              content: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      hintText: 'Email',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
                     ),
                   ),
-                )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.home),
+                      hintText: 'Address',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      prefixIcon: Icon(Icons.phone),
+                      hintText: 'Mobile No',
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Step(
-                title: Text('Upload'),
-                content: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    hintText: 'Mobile Number',
-                    prefixIcon: Icon(
-                      Icons.phone,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ))
+              title: Text('Upload'),
+              content: Text(
+                'Registration Successfully Completed !!',
+                style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),
+              ),
+            ),
           ],
         ),
       ),
