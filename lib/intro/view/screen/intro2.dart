add170805ae448ce14e12f2c93538b2ebd24a0d7
Dell <5095meshwapatel@email.com>
2024-05-01 11:25:25 +0530

    ram

--- lib/intro/view/screen/intro2.dart
+++ lib/intro/view/screen/intro2.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 class Intro2 extends StatelessWidget {
   const Intro2({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
